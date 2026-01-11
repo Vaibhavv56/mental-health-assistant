33212a9469e1376d37c2aa1d2152af1ca34f701b
Vaibhav Pujari <itsvaibhavpujari@gmail.com>
2026-01-11 10:09:45 +0530

    Add webpack alias configuration for @ path resolution

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
 
